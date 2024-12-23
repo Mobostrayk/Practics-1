43b27417746c58709ba5097da47890ff55ee7a94
Елисей Чубукин <e-chubukin2017@yandex.ru>
2024-12-23 23:26:24 +0700

    Конец

--- Presenter1/Start.cs
+++ Presenter1/Start.cs
@@ -11,28 +11,43 @@ namespace Presenter1
     internal class Start
     {
         static void Main(string[] args)
-        {
-            //ВинФорма
-            //Form1 view = new Form1();
-            //IKernel ninjectKernel = new StandardKernel(new SimpleConfigModule());
-            //ILogic logic = ninjectKernel.Get<Logic>();
+        {           
 
-            //Консоль
-            var view = new DecanatPro();
-            IKernel ninjectKernel = new StandardKernel(new SimpleConfigModule());
-            ILogic logic = ninjectKernel.Get<Logic>();
+            bool isCommandSelected = false;
 
+            while (!isCommandSelected)
+            {
+                Console.WriteLine("Выберите команду:");
+                Console.WriteLine("Введите 'console' для выполнения консольного кода.");
+                Console.WriteLine("Введите 'form' для выполнения кода формы.");
 
-            new Presenter1(view, logic);
+                string userInput = Console.ReadLine();
 
-            //ВинФорма
-            //StarterForm.StartForm(view);
+                switch (userInput.ToLower())
+                {
+                    case "console":
+                        var view = new DecanatPro();
+                        IKernel ninjectKernel = new StandardKernel(new SimpleConfigModule());
+                        ILogic logic = ninjectKernel.Get<Logic>();
+                        new Presenter1(view, logic);
+                        StarterСonsole.StartConsole(view);
 
-            //Консоль
-            StarterСonsole.StartConsole(view);
-
-
-            Console.ReadKey();
+                        isCommandSelected = true; // После выбора задачи, выходим из цикла
+                        break;
+                    case "form":
+                        Form1 view2 = new Form1();
+                        IKernel ninjectKernel2 = new StandardKernel(new SimpleConfigModule());
+                        ILogic logic2 = ninjectKernel2.Get<Logic>();
+                        new Presenter1(view2, logic2);
+                        StarterForm.StartForm(view2);
+                        isCommandSelected = true; // После выбора задачи, выходим из цикла
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели некорректный ввод. Пожалуйста, выберите 'console' или 'form'.");
+                        break;
+                }
+            }
+            Console.WriteLine("Программа завершена.");
         }
     }
 }
